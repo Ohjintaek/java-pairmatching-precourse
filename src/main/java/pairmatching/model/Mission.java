ae1e999c5f15b27763fdc4e5c57039a3f767d441
OhJintaek <sunshot@hanyang.ac.kr>
2022-12-15 23:59:09 +0900

    feat: 미션을 관리하는 Mission enum 추가

--- src/main/java/pairmatching/model/Mission.java
+++ src/main/java/pairmatching/model/Mission.java
@@ -1,2 +1,20 @@
-package pairmatching.model;public class MIssion {
+package pairmatching.model;
+
+public enum Mission {
+    CAR_RACING("자동차경주", Level.LEVEL1),
+    LOTTO("로또", Level.LEVEL1),
+    NUMBER_BASEBALL("숫자야구", Level.LEVEL1),
+    SHOPPING_BASKET("장바구니", Level.LEVEL2),
+    PAYMENT("결제", Level.LEVEL2),
+    SUBWAY_MAP("지하철노선도", Level.LEVEL2),
+    PERFORMANCE_IMPROVEMENT("성능개선", Level.LEVEL4),
+    DISTRIBUTION("배포", Level.LEVEL4);
+
+    private String name;
+    private Level level;
+
+    Mission(String name, Level level) {
+        this.name = name;
+        this.level = level;
+    }
 }
